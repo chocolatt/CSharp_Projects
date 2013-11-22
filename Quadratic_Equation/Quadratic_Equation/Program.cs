278e58d5ca619ba3077898d5a8206df6fc0e8c00
unknown <olga.tkachuk@lwo1-dnn-f12715.synapse.com>
2013-11-22 16:58:23 +0200

    QuadraticEquation folder added

---
+++ Quadratic_Equation/Quadratic_Equation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quadratic_Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please, enter there parametrs -a, b and c ");
+            var result = QE.QuadraticEquation(1, -2, 7);
+            // 2,3,1 - (-0,5;1) //1,5,6 - (-2;-3) // -9,6,-1 - (0,(3)) //1,-2,7 - (no determination)
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quadratic_Equation/Quadratic_Equation/QE.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quadratic_Equation
+{
+    class QE
+    {
+        public static double[] QuadraticEquation(int a, int b, int c)
+        {
+            {
+                double D;
+
+                if (a != 0)
+                {
+                    D = b * b - 4 * a * c;
+                    if (D < 0)
+                    {
+                        //if D is < 0 than QuadraticEquation has no determination
+                        return new double[0];
+                    }
+                    else if (D == 0)
+                    {
+                        //if D = 0, there is always one result 
+                        return new double[1] { -b / (2 * a) };
+                    }
+                    else
+                    {
+                        var x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                        var x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                        //now let's check if recieved results satisfy our equation --- ????? ти собі не довіряєш?
+                        return new double[] { x1, x2 };
+                    }
+                }
+                else if (a == 0 && b != 0)
+                {
+                    // b*x + c = 0
+                    return new double[1] { -c / b };
+                }
+                else
+                    // НЕ ТРЕБА НІЧОГО ПИСАТИ В КОНСОЛЬ! ЦЕЙ МЕТОД РОЗВ"ЯЗУЄ РІВНЯННЯ, А НЕ ГАДИТЬ КУДИСЬ   Console.WriteLine("Given QuadraticEquation has no determination");
+                    return new double[0];
+            }
+        }
+    }
+}
